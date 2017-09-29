f669aa318a10d3ff8f84461733594fba499536ad
antoni-heredia <a.heredia.castillo@gmail.com>
2017-09-29 12:52:23 +0200

    Completado hasta el metodo 1 (Practica1)

--- QytetetJava/src/modeloqytetet/PruebaQytetet.java
+++ QytetetJava/src/modeloqytetet/PruebaQytetet.java
@@ -14,17 +14,39 @@ public class PruebaQytetet {
     /**
      * @param args the command line arguments
      */
+
     private static ArrayList<Sorpresa> mazo = new ArrayList();
     
     private static void inicializarSorpresa(){
         mazo.add(new Sorpresa("Te hemos pillado con chanclasy calcetines, "
-                + "lo sentimos, ¡Debes ir a la carcel!",0, 
+                + "lo sentimos, ¡Debes ir a la carcel!",9, 
                 TipoSorpresa.IRACASILLA));
+        mazo.add(new Sorpresa("Un fan anonimo a pagado tu fianza. "
+                + "Sales de la carcel.", 0, TipoSorpresa.SALIRCARCEL));
+        mazo.add(new Sorpresa("Has suspendido PDOO en la convocatoria "
+                + "extraordinaria.¡Paga doble matricula!", -130, 
+                TipoSorpresa.PAGARCOBRAR));
+        mazo.add(new Sorpresa("Has conseguido una matricula de honor. "
+                + "¡ENHORABUENA EMPOLLON!. Te devuelven dinero", 60, 
+                TipoSorpresa.PAGARCOBRAR));
+    }
+    
+    private static ArrayList<Sorpresa> getSorpresasValorPositivo(){
+        ArrayList<Sorpresa> sorpresas_positiva = new ArrayList();
+        for(Sorpresa s : mazo){
+            if(s.getValor() > 0)
+                sorpresas_positiva.add(s);
+        }
+        return sorpresas_positiva;
     }
     
     public static void main(String[] args) {
         // TODO code application logic here
         
+        PruebaQytetet.inicializarSorpresa();
+        
+        for(Sorpresa s : mazo){
+            System.out.println(s.toString());
+        }
     }
-    
 }
