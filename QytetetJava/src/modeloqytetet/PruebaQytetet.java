db1c0a8aa7bdff84b793ce9b5ee47fcd608cac44
antoni-heredia <a.heredia.castillo@gmail.com>
2017-09-29 11:56:56 +0200

    Renombrado los paquetes y la clase principal 	modificado:    .gitignore 	modificado:    QytetetJava/nbproject/project.properties 	renombrado:    QytetetJava/src/qytetetjava/QytetetJava.java -> QytetetJava/src/modeloqytetet/PruebaQytetet.java 	borrado:       README.md

--- QytetetJava/src/modeloqytetet/PruebaQytetet.java
+++ QytetetJava/src/modeloqytetet/PruebaQytetet.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package qytetetjava;
+package modeloqytetet;
 
 /**
  *
  * @author antonio
  */
-public class QytetetJava {
+public class PruebaQytetet {
 
     /**
      * @param args the command line arguments
--- README.md
+++
@@ -1,2 +0,0 @@
-# Qytetet
-Repositorio donde se encuentra la practica de PDOO basada en el Monopoli
